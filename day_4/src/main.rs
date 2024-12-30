d6c835c79446d9316feed801df2f814a30115e93
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-12-30 06:28:43 +0100

    all ways are implemented

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -70,14 +70,137 @@ fn main() {
                     }
                 }
                 // CASE 3: Go right
+                if charItter <= to_be_investigated.len() - 5 {
+                    if to_be_investigated[charItter+1] == "M" {
 
+                        if to_be_investigated[charItter+2] == "A" {
+                            if  to_be_investigated[charItter+3] == "S" {
+                                counter += 1;
+                                println!("Crazy. XMAS is against right")
+                            }
+                        }
+                    }
+                }
                 // CASE 4: Go left
+                if charItter >= 3 {
+                    if to_be_investigated[charItter-1] == "M" {
 
-                // Case 5: Go diagonal right
+                        if to_be_investigated[charItter-2] == "A" {
+                            if  to_be_investigated[charItter-3] == "S" {
+                                counter += 1;
+                                println!("Crazy. XMAS is against left")
+                            }
+                        }
+                    }
+                }
+                // Case 5: Go diagonal right down
+                if lineItter <= file_content.len() - 5 &&  charItter <= to_be_investigated.len() - 5{
+                    let oneCharDown: Vec<String> = file_content[lineItter + 1]
+                        .split("")
+                        .map(|x| x.to_string())
+                        .collect();
 
-                // CASE 6: Go diagonal left
+                    if oneCharDown[charItter + 1] == "M" {
+                        let twoCharsDown: Vec<String> = file_content[lineItter + 2]
+                            .split("")
+                            .map(|x| x.to_string())
+                            .collect();
+
+                        if twoCharsDown[charItter + 2] == "A" {
+                            let threeCharsDown: Vec<String> = file_content[lineItter + 3]
+                                .split("")
+                                .map(|x| x.to_string())
+                                .collect();
+
+                            if threeCharsDown[charItter + 3] == "S" {
+                                counter += 1;
+                                println!("Crazy. XMAS is against down left")
+                            }
+                        }
+                    }
+                }
+                // CASE 6: Go diagonal left down
+
+                if lineItter <= file_content.len() - 5 &&  charItter >= 3{
+                    let oneCharDown: Vec<String> = file_content[lineItter + 1]
+                        .split("")
+                        .map(|x| x.to_string())
+                        .collect();
+
+                    if oneCharDown[charItter - 1] == "M" {
+                        let twoCharsDown: Vec<String> = file_content[lineItter + 2]
+                            .split("")
+                            .map(|x| x.to_string())
+                            .collect();
+
+                        if twoCharsDown[charItter - 2] == "A" {
+                            let threeCharsDown: Vec<String> = file_content[lineItter + 3]
+                                .split("")
+                                .map(|x| x.to_string())
+                                .collect();
+
+                            if threeCharsDown[charItter - 3] == "S" {
+                                counter += 1;
+                                println!("Crazy. XMAS is against down right")
+                            }
+                        }
+                    }
+                    // Case 7: Go diagonal right up
+                    if lineItter >= 3 &&  charItter <= to_be_investigated.len() - 5{
+                        let oneCharDown: Vec<String> = file_content[lineItter - 1]
+                            .split("")
+                            .map(|x| x.to_string())
+                            .collect();
+
+                        if oneCharDown[charItter + 1] == "M" {
+                            let twoCharsDown: Vec<String> = file_content[lineItter - 2]
+                                .split("")
+                                .map(|x| x.to_string())
+                                .collect();
+
+                            if twoCharsDown[charItter + 2] == "A" {
+                                let threeCharsDown: Vec<String> = file_content[lineItter - 3]
+                                    .split("")
+                                    .map(|x| x.to_string())
+                                    .collect();
+
+                                if threeCharsDown[charItter + 3] == "S" {
+                                    counter += 1;
+                                    println!("Crazy. XMAS is against above left")
+                                }
+                            }
+                        }
+                    }
+                // CASE 6: Go diagonal left down
+
+                if lineItter >= 3 &&  charItter >= 3{
+                    let oneCharDown: Vec<String> = file_content[lineItter - 1]
+                        .split("")
+                        .map(|x| x.to_string())
+                        .collect();
+
+                    if oneCharDown[charItter - 1] == "M" {
+                        let twoCharsDown: Vec<String> = file_content[lineItter - 2]
+                            .split("")
+                            .map(|x| x.to_string())
+                            .collect();
+
+                        if twoCharsDown[charItter - 2] == "A" {
+                            let threeCharsDown: Vec<String> = file_content[lineItter - 3]
+                                .split("")
+                                .map(|x| x.to_string())
+                                .collect();
+
+                            if threeCharsDown[charItter - 3] == "S" {
+                                counter += 1;
+                                println!("Crazy. XMAS is against above right")
+                            }
+                        }
+                    }
+                }
             }
         }
     }
+}
     println!("{}", counter)
 }
